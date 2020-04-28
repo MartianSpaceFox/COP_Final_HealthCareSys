ee21c9103ac6267eb7d8fa4b02d17562e904ba5f
MartianSpaceFox <jessnacht@outlook.com>
2020-04-28 19:28:47 +0000

    Added read from excel file code.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,49 @@
+//Libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
 
+using namespace std;
+//Header files
+#include "PatientAccount.h"
+#include "Pharmacy.h"
+#include "Surgery.h"
+
+
+
+int main()
+{
+ifstream inFile;
+
+string procedureID;
+string description;
+string amount;
+
+ 
+inFile.open("jfkmedicalcenter_cm.csv");
+if (inFile.is_open()) {
+    cout << "File has been opened" << endl;
+}
+else {
+    cout << "NO FILE HAS BEEN OPENED" << endl;
+}
+
+
+while (!inFile.eof()) {
+    getline ( inFile, procedureID, ',' );
+    getline ( inFile, description, ',' );
+    getline ( inFile, amount, ',' );
+
+    cout << endl;
+    cout << "Procedure ID: " << procedureID << endl;
+    cout << "Description: " << description << endl;
+    cout << "Amount: " << "$" << amount << endl;
+    cout << endl;
+}
+
+inFile.close();
+system("pause");
+return 0;
+
+} 
